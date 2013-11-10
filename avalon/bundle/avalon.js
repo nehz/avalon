40d796d05f92a569da93b7b1e8bfceb27618ba52
Zhen Wang <ichigo.san@gmail.com>
2013-11-10 16:51:58 +0800

    Repeat bind minimal working

--- avalon/bundle/avalon.js
+++ avalon/bundle/avalon.js
@@ -17,19 +17,26 @@
 
     /* Model */
 
-    var Observable = avalon.Observable = function() {
-        var value;
+    var Observable = avalon.Observable = function(v) {
         var listeners = [];
+        this.value = v;
 
         //TODO: cleanup listeners
         this.listen = function(f) {
             listeners.push(f);
+            return f;
         };
         this.bind = function(v) {
-            value = v;
+            if (v == this.value) {
+                return v;
+            }
+
+            this.value = v;
             $.each(listeners, function(i, f) {
                 f(v);
             });
+
+            return v;
         };
     };
 
@@ -47,8 +54,91 @@
             };
 
             $(root).html($template.html());
-            avalon.bootstrap(root, context);
-            return context;
+            return avalon.bootstrap(root, context);
+        };
+    };
+
+    var Repeat = avalon.Repeat = function(root, v, context) {
+        var $root = $(root);
+        var bind = $root.prop('bind');
+
+        var value = v;
+        var repeat = [];
+        var contexts = context[bind + '_repeat'] = [];
+
+        function create(index) {
+            var r = $root.prop('template').clone();
+            contexts[index] = avalon.bootstrap(r, {
+                _parent: context,
+                _template: template,
+                _index: new Observable(index)
+            });
+            return r;
+        }
+
+        var last = $root;
+        for (var i = 0; i < v.length; i++) {
+            var r = create(i);
+            repeat.push(r);
+            last = r.insertAfter(last);
+        }
+
+        this.merge = function(v) {
+            if (value == v) {
+                return;
+            }
+
+            var removed = [];
+            var last = $root;
+
+            if (!v.length) {
+                removed = value;
+            }
+
+            for(var i = 0; i < v.length; i++) {
+                if (value.length <= i) {
+                    last = create(i).insertAfter(last);
+                    repeat.push(last);
+                    value.push(v[i]);
+                }
+                else if(value[i] !== v[i]) {
+                    if (value.length <= v.length) {
+                        contexts.splice(i, 0, undefined);
+                        last = create(i).insertAfter(last);
+                        repeat.splice(i, 0, last);
+                        value.splice(i, 0, v[i]);
+                    }
+                    else {
+                        last = repeat[i];
+                        removed.push(repeat.splice(i, 1)[0]);
+                        value.splice(i, 1);
+                        contexts.splice(i, 1);
+                    }
+                }
+                else {
+                    last = repeat[i];
+                }
+                contexts[i]._index.bind(i);
+            }
+
+            removed.push.apply(
+                removed,
+                repeat.splice(i, repeat.length)
+            );
+            value.splice(i, value.length);
+            contexts.splice(i, contexts.length);
+
+            for (var i = 0; i < removed.length; i++) {
+                removed[i].remove();
+            }
+        };
+
+        this.destroy = function() {
+            // TODO: cleanup child scopes
+            for (var i = 0; i < value.length; i++) {
+                repeat[i].remove();
+            }
+            delete context[bind + '_' + i];
         };
     };
 
@@ -87,11 +177,18 @@
     };
 
     avalon.bootstrap = function(root, context) {
+        if (context == undefined) {
+            console.error('No context supplied for', root);
+            return undefined;
+        }
+
         var templateId = 0;
         root = $(root);
-        context = context || avalon.context;
 
         $.each(root.children(), function(i, c) {
+            var $c = $(c);
+            var leaf = $c.contents().length == 0;
+            
             // Get binding
             var bind;
             for (var i = 0; i < c.attributes.length; i++) {
@@ -111,45 +208,103 @@
             }
 
             var o = get(context, bind);
+            if (o == undefined) {
+                o = set(context, bind, new Observable())
+            }
+
             if (o instanceof Template) {
                 // Template bind
-                bind = o.name + '_' + ($(c).attr('id') || templateId++);
+                bind = o.name + '_' + ($c.attr('id') || templateId++);
                 context[bind] = o.render(c, context);
             }
-            else if(!o || o instanceof Observable) {
-                if (!o) {
-                    o = set(context, bind, new Observable())
-                }
-
-                // Input bind
-                if ($(c).is('input')) {
-                    $(c).on('input keypress keyup', function(e) {
-                        if (e.type == 'keyup' && e.keyCode != 8)  {
-                            return;
-                        }
-                        o.bind($(c).val());
-                    });
-                    $(c).on('cut paste', function() {
+            else if(o instanceof Observable) {
+                if ($c.is('input')) {
+                    // Input bind
+                    $c.on('input keypress cut paste show', function() {
                         defer(function() {
-                            o.bind($(c).val());
+                            o.bind($c.val());
                         });
                     });
-                    o.listen(function(v) {
-                        $(c).val(v);
+                    $c.on('keydown keyup', function(e) {
+                        if (e.keyCode == 8)  {
+                            defer(function() {
+                                o.bind($c.val());
+                            });
+                        }
                     });
+                    (o.listen(function(v) {
+                        $c.val(v);
+                    }))(o.value);
                 }
                 else {
-                    if (context[bind] instanceof Array) {
-                        // TODO
+                    // Display bind
+                    $c.prop('template', $c.clone());
+                    $c.prop('display', $c.css('display'));
+                    $c.prop('bind', bind);
+
+                    if (!o.value) {
+                        avalon.bootstrap($c, context);
                     }
-                    o.listen(function(v) {
+
+                    (o.listen(function(v) {
+                        if (!v && !leaf) {
+                            $c.css('display', 'none');
+                            // TODO: cleanup repeat
+
+                            var repeat = $c.prop('repeat');
+                            if (repeat) {
+                                $c.removeProp('repeat');
+                                repeat.destroy();
+                            }
+                            return;
+                        }
+
+                        // TODO: refactor, also own repeat context (rework)
                         if (v instanceof Array) {
-                            console.log('repeat');
+                            $c.css('display', 'none');
+
+                            var repeat = $c.prop('repeat');
+                            if (!repeat) {
+                                $c.prop('repeat', new Repeat($c, v, context));
+                            }
+                            else {
+                                repeat.merge(v);
+                            }
                         }
                         else {
-                            $(c).text(v);
+                            $c.css('display', $c.prop('display'));
+
+                            //TODO: clean up code
+                            //TODO: repeated code, refactor
+                            var repeat = $c.prop('repeat');
+                            if (repeat) {
+                                $c.removeProp('repeat');
+                                repeat.destroy();
+                            }
+
+                            // Bind text value if no children
+                            if (leaf) {
+                                $c.text(v);
+                            }
+                            else {
+                                $c.html($c.prop('template').html());
+                                avalon.bootstrap($c, context);
+                            }
                         }
-                    });
+                    }))(o.value);
+                }
+            }
+            else if(typeof(o) == 'number' || typeof(o) == 'string') {
+                if (leaf) {
+                    $c.text(o);
+                }
+                else {
+                    if (o) {
+                        avalon.bootstrap($c, context);
+                    }
+                    else {
+                        $c.prop('display', $c.css('display'));
+                    }
                 }
             }
             else {
@@ -159,13 +314,14 @@
                 })
             }
         });
+        return context;
     };
 
 
     /* Load */
 
     $(function() {
-        avalon.bootstrap($('#avalon-root'));
+        avalon.bootstrap($('#avalon-root'), avalon.context);
     });
 
     return avalon;
