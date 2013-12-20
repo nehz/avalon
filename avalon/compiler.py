b78ee739ed060b14b098fb263f2e9dc7cfea0a5e
Zhen Wang <ichigo.san@gmail.com>
2013-12-20 23:06:53 +0800

    Compiler: change to 2 spaces per indent

--- avalon/compiler.py
+++ avalon/compiler.py
@@ -228,7 +228,7 @@ class JSCompiler(ast.NodeVisitor):
 # Helpers
 #==============================================================================
 
-def indent(lines, spaces=4, level=1):
+def indent(lines, spaces=2, level=1):
     spaces = ' ' * (spaces * level)
     if isinstance(lines, list):
         return  ['{0}{1}'.format(spaces, l) for l in lines]
