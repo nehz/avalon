33649ac1d07518db52661839b8d41ed5c2aeb869
nehz <ichigo.san@gmail.com>
2014-02-25 02:34:56 +0800

    Compiler: flatten exception handler

--- avalon/compiler/compiler.py
+++ avalon/compiler/compiler.py
@@ -716,26 +716,30 @@ class JSCompiler(ast.NodeVisitor):
 
     # ExceptHandler(expr? type, identifier? name, stmt* body)
     def visit_ExceptHandler(self, node):
+        if not node.branch:
+            raise SyntaxError('Except block not inside a function block')
+
         tpl = []
+        continue_point = node.branch.create()
         if node.type:
-            tpl.append('if ($exception instanceof {0}) {{'.format(
-                self.visit(node.type)))
+            extend(tpl, 'if (!($exception instanceof {0})) {1};',
+                   self.visit(node.type), goto(continue_point))
         else:
-            tpl.append('if ($exception) {')
+            extend(tpl, 'if (!($exception)) {0};', goto(continue_point))
 
         if node.name:
             if node.context:
-                extend(tpl, '  {0}.{1} = $exception;', node.context, node.name)
+                extend(tpl, '{0}.{1} = $exception;', node.context, node.name)
             else:
-                extend(tpl, '  var {0} = $exception;', node.name)
+                extend(tpl, 'var {0} = $exception;', node.name)
 
         for c in node.body:
-            extend(tpl, indent(self.visit(c)))
+            extend(tpl, self.visit(c))
 
-        tpl.append(indent('$exception = undefined;'))
-        tpl.append('}')
-
-        return tpl
+        return extend(tpl, [
+            '$exception = undefined;',
+            label(continue_point)
+        ])
 
     # arg = (identifier arg, expr? annotation)
     def visit_arg(self, node):
